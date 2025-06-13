21d695c19987c4058f7acf704072c86480381c3d
Robert G. Jakabosky <rjakabosky+neopallium@neoawareness.com>
2025-06-13 18:26:22 +0800

    Improve CurveTree error type.

--- relations/src/error.rs
+++ relations/src/error.rs
@@ -1,13 +1,36 @@
 use ark_serialize::SerializationError;
 use schnorr_pok::error::SchnorrError;
+use thiserror::Error;
 
-#[derive(Debug)]
+pub type Result<T> = core::result::Result<T, Error>;
+
+#[derive(Debug, Error)]
 pub enum Error {
+    /// Curve Tree height cannot be 0
+    #[error("Curve Tree height cannot be 0")]
     HeightCantBe0,
+
+    /// Leaf doesn't exist at the given index
+    #[error("Leaf doesn't exist at index {0}")]
     LeafDoesntExistAtIndex(u64),
+
+    /// Leaf already exists at the given index
+    #[error("Leaf already exists at index {0}")]
     LeafAlreadyExistAtIndex(u64),
+
+    /// Leaf index is not expected
+    #[error("Leaf index {0} is not expected, expected {1}")]
     LeafIndexNotAsExpected(u64, u64),
+
+    /// Leaf index is not in the expected range
+    #[error("Leaf index {1} is not in the expected range [0, {0})")]
     TreeWontSupportRequiredInsertions(u64, u64),
-    SerializationError(SerializationError),
+
+    /// Serialization error
+    #[error("Serialization error: {0}")]
+    SerializationError(#[from] SerializationError),
+
+    /// Schnorr proof error
+    #[error("Schnorr proof error: {0:?}")]
     SchnorrError(SchnorrError),
 }
